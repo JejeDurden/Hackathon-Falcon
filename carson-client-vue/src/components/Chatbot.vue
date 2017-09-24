ea8120d3cf25fcbf3eba122d234a4ba3644babe4
malikfassi <malikfassi@users.noreply.github.com>
2017-09-24 13:30:53 +0200

    update

--- carson-client-vue/src/components/Chatbot.vue
+++ carson-client-vue/src/components/Chatbot.vue
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div id="inputZone">
-      <img class="inputElement inputButton" src="../assets/speak.png"/>
+      <img class="inputElement inputButton" @click="recording ? stopRecording : startRecording" src="../assets/speak.png"/>
       <div class="inputElement" id="inputWrap"><input id="theInput" @keyup.enter="sendMessage" placeholder="Ask me anything"/></div>
       <img class="inputElement inputButton" id="send" src="../assets/send.svg" @click="sendMessage"/>
     </div>
@@ -19,13 +19,15 @@
 </template>
 
 <script>
+/* eslint-disable */
 const $ = require('jQuery')
+const Recorder = require('../../dist/recorder.js')
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      loading: true,
+      loading: false,
       messages: [
         {
           from: 0, // CARSON
@@ -47,8 +49,27 @@ export default {
           from: 0, // CARSON
           content: 'Tres bien.'
         }
-      ]
+      ],
+      recorder: null,
+      audio_context: null,
+      recording: false
     }
+  },
+  mounted: function () {
+    const vm = this
+    vm.scrollToBottom('#messageZone')
+    try {
+      // webkit shim
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+      window.URL = window.URL || window.webkitURL;
+      vm.audio_context = new AudioContext;
+    } catch (e) {
+      alert('No web audio support in this browser!');
+    }
+    navigator.getUserMedia({audio: true}, vm.startUserMedia, function(e) {
+      console.log("fail")
+    });
   },
   updated: function () {
     const vm = this
@@ -61,13 +82,78 @@ export default {
       if (data) {
         $('#theInput').val('')
         vm.messages.push({from: 1, content: data})
+        vm.loading = true
+        $.get(encodeURI('http://b99052d7.ngrok.io/api/converse?text=' + data), function (result) {
+          let resp = JSON.parse(result)
+          vm.loading = false
+          vm.messages.push({from: 0, content: resp.response || 'ERROR'})
+        })
       }
     },
     scrollToBottom: function (id) {
       var div = $(id)
       var height = div[0].scrollHeight
       div.scrollTop(height)
-    }
+    },
+    createAudioElement: function (blobUrl) {
+        const downloadEl = document.createElement('a');
+        downloadEl.style = 'display: block';
+        downloadEl.innerHTML = 'download';
+        downloadEl.download = 'audio.webm';
+        downloadEl.href = blobUrl;
+        const audioEl = document.createElement('audio');
+        audioEl.controls = true;
+        const sourceEl = document.createElement('source');
+        sourceEl.src = blobUrl;
+        sourceEl.type = 'audio/webm';
+        audioEl.appendChild(sourceEl);
+        document.body.appendChild(audioEl);
+        document.body.appendChild(downloadEl);
+    },
+    startUserMedia: function (stream) {
+      const vm = this;
+      console.log("starting Media")
+      var input = vm.audio_context.createMediaStreamSource(stream);
+      // Uncomment if you want the audio to feedback directly
+      //input.connect(audio_context.destination);
+      //__log('Input connected to audio context destination.');
+      vm.recorder = new Recorder(input);
+    },
+    startRecording: function (button) {
+      const vm = this;
+      console.log("start record")
+      vm.recording = true
+      vm.recorder && vm.recorder.record();
+    },
+    stopRecording: function (button) {
+      const vm = this;
+      console.log("stoping")
+      vm.recorder && vm.recorder.stop();
+      vm.recording = false;
+      button.disabled = true;
+      
+      // create WAV download link using audio data blob
+      vm.createDownloadLink();
+      vm.recorder.clear();
+    },
+    createDownloadLink: function () {
+      vm.recorder && vm.recorder.exportWAV(function(blob) {
+      var url = URL.createObjectURL(blob);
+      var li = document.createElement('li');
+      var au = document.createElement('audio');
+      var hf = document.createElement('a');
+      
+      au.controls = true;
+      au.src = url;
+      hf.href = url;
+      hf.download = new Date().toISOString() + '.wav';
+      hf.innerHTML = hf.download;
+      li.appendChild(au);
+      li.appendChild(hf);
+      recordingslist.appendChild(li);
+    })
+  }
+
   }
 }
 </script>
@@ -76,18 +162,26 @@ export default {
 <style scoped lang="SASS">
 $black : #151515;
 $gold : rgb(195, 156, 104);
+@font-face {
+  font-family: "Museo";
+  src: url('../assets/Museo.otf') format('truetype')
+}
 textarea:focus, input:focus{
-    outline: none;
+  outline: none;
+}
+input {
+  border: none;
 }
 .body {
   position: absolute;
   top: 10px;
   left: 10px;
   right: 10px;
-  bottom: 10px;
+  bottom: 0px;
+  font-family: 'Museo';
   #messageZone {
-    margin-top: 150px;
-    height: 440px;
+    margin-top: 115px;
+    height: calc(100% - 202px);
     overflow-y: scroll;
     .messagesWrapper {
     }
---
+++ carson-hotesse-vue/src/components/Hello.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div id="body">
+      <img :src="imagePath(tab)"/>
+    </div>
+    <div id="nav">
+      <div :class="'navWrap' + (tab == 1 ? ' active' : '')">
+        <img src="../assets/icon1.png" @click="changeTab(1)"/>
+      </div>
+      <div :class="'navWrap' + (tab == 2 ? ' active' : '')">
+        <img src="../assets/icon2.png" @click="changeTab(2)"/>
+      </div>
+      <div :class="'navWrap' + (tab == 3 ? ' active' : '')">
+        <img src="../assets/icon3.png" @click="changeTab(3)"/>
+      </div>
+      <div :class="'navWrap' + (tab == 4 ? ' active' : '')">
+        <img src="../assets/icon4.png" @click="changeTab(4)"/>
+      </div>
+      <div :class="'navWrap' + (tab == 5 ? ' active' : '')">
+        <img src="../assets/icon5.png" @click="changeTab(5)"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      tab: 1
+    }
+  },
+  methods: {
+    changeTab: function (nb) {
+      const vm = this
+      vm.tab = nb
+    },
+    imagePath: function (nb) {
+      return require('../assets/page' + this.tab + '.png')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="SASS">
+
+$gold : rgb(195, 156, 104);
+
+@font-face {
+  font-family: "Museo";
+  src: url('../assets/Museo.otf') format('truetype')
+}
+
+#nav {
+  position: sticky;
+  bottom: 0px;
+  width: 100%;
+  background-color: white;
+  .navWrap {
+    display: inline-block;
+    padding: 20px 40px;
+    img {
+      width: 100px;
+    }
+  }
+  .active {
+    background-color: $gold;
+  }
+}
+#body {
+  width: 100%;
+  height: 100%;
+  img {
+    width: 100%;
+  }
+}
+</style>
